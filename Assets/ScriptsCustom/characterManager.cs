c3214dba93f9263a6746a5612aecb2a11a817339
Izzie Colpitts-Campbell <izzieccampbell@gmail.com>
2015-08-14 12:27:02 -0400

    Revert "no message"
    This reverts commit 01c0cdb9704c7f379cbb1bda3db006e5389bbdc1.

---
+++ Assets/ScriptsCustom/characterManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+//using Windows.Kinect;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text; 
+
+public class CharacterManager : MonoBehaviour
+{
+
+    //created player prefab
+    public GameObject playerPrefab;
+    public int numCharacters = 0;
+
+    private List<GameObject> characters;
+
+    private static CharacterManager instance = null;
+    public static CharacterManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        numCharacters = 0;
+        characters = new List<GameObject>();
+        instance = this;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        KinectManager manager = KinectManager.Instance;
+
+        if(numCharacters < manager.GetUsersCount())
+        {
+            GameObject character = Instantiate<GameObject>(playerPrefab);
+            character.GetComponent<NetworkedUserController>().playerIndex = manager.GetUsersCount() - 1;
+            characters.Add(character);
+
+            numCharacters = manager.GetUsersCount();
+
+        }
+
+    }
+}
